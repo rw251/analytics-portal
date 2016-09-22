8c7e8c2345401dd3731826eedfdac514fa71c67b
Richard Williams <richard.williams2@manchester.ac.uk>
2016-09-22 11:23:27 +0100

    Moved config to environment variables

---
+++ server/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  mysql: {
+    host: process.env.MUJO_MYSQL_HOST,
+    user: process.env.MUJO_MYSQL_USER,
+    password: process.env.MUJO_MYSQL_PASSWORD,
+    database: process.env.MUJO_MYSQL_DB
+  },
+  passport: {
+    secret: process.env.MUJO_PASSPORT_SECRET
+  },
+  server: {
+    port: process.env.MUJO_SERVER_PORT
+  },
+  mongo: {
+    url: process.env.MUJO_MONGO_URL
+  }
+};
